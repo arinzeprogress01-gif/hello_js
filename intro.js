e4146d0b81052dbb608ffc0046362e645faf4e40
Nnoli Higgs <arinzeprogress01@gmail.com>
2026-02-12 10:39:46 +0100

    improve code

--- intro.js
+++ intro.js
@@ -1,4 +1,4 @@
 console.log("Hello from Node");
 console.log("Nnoli Arinze Progress")
 console.log("i am 25 years old")
-console.log("favourite Programming language : Pyhton")
+console.log("favourite Programming language :Pyhton")
