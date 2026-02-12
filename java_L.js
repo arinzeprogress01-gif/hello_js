d29e26c3c04ff60eef0bcbe909e5277965ff6556
Nnoli Higgs <arinzeprogress01@gmail.com>
2026-02-12 09:18:16 +0100

    initial commit: The First Higgs Project

---
+++ java_L.js
@@ -0,0 +1,7 @@
+
+console.log(`Items (${1 + 1}) : $${(2890 + 799) / 100}`);
+console.log(Math.round(2.894));
+console.log('hello');
+console.log(`Items (${5 + 5 + '%'}) : $${Math.round(678.6) / 100}`);
+
+
